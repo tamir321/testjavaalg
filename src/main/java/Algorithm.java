690a1ece2e3cda8b2be5d9e6e0755937ee71cf49
tamir reiss <tamir321@gmail.com>
2020-10-26 14:41:08 +0200

    first commit

---
+++ src/main/java/Algorithm.java
@@ -0,0 +1,70 @@
+
+import hkube.algo.wrapper.IAlgorithm;
+import hkube.api.IHKubeAPI;
+import hkube.api.INode;
+import org.json.JSONObject;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
+
+public class Algorithm implements IAlgorithm {
+
+    @Override
+    public void Init(Map args) {
+
+    }
+
+
+    @Override
+    public Object Start(Map args, IHKubeAPI hkubeAPI) throws Exception {
+
+
+        Collection<Integer> input = (Collection<Integer>)args.get("input");
+
+        INode node = new INode() {
+            @Override
+            public String getName() {
+                return "yellow-alg-NOde";
+            }
+
+            @Override
+            public JSONObject[] getInput() {
+                return new JSONObject[0];
+            }
+
+            @Override
+            public void setInput(JSONObject[] input) {
+
+            }
+            @Override
+            public String getAlgorithmName() {
+                return "yellow-alg";
+            }
+
+            @Override
+            public void setAlgorithmName(String algorithmName) {
+
+            }
+        };
+
+
+        return input;
+    }
+
+
+
+
+
+
+    @Override
+    public void Stop() {
+
+    }
+
+    @Override
+    public void Cleanup() {
+
+    }
+
+}
